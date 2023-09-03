f19a7e97dfe1e4be682fc638482fc5f053b325b6
grazn <graznyjhazain@gmail.com>
2023-09-03 15:08:23 +0300

    Update app

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,5 +5,14 @@ public class Main {
         int n = 6;
         int sum = 0;
 
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+            System.out.println(i + ") Num is " + i + ", sum is " + sum);
+        }
+
+        System.out.println("------------------------");
+        System.out.println("Sum of numbers is " + sum);
+    }
+}
 
 
